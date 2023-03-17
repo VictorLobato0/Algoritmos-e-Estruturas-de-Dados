fe9e0493da4bfc5b7d976185bcac351ee54d3ac3
Victor Emanuel Lobato <92333133+VictorLobato0@users.noreply.github.com>
2023-03-17 00:17:48 -0300

    add files

---
+++ atividade_4/atividade_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Criação do objeto para uso da classe triangulo
+            triangulo blastoise = new triangulo();
+
+
+            Console.WriteLine("Digite um valor para a:");
+            blastoise.setA(float.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite um valor para b:");
+            blastoise.setB(float.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite um valor para a:");
+            blastoise.setC(float.Parse(Console.ReadLine()));
+
+            blastoise.imprimirDados();
+
+
+
+
+        }
+    }
+}
